794b94b08132dd4a8f359d3478747f86c9243295
baysesss <73853610+baysesss@users.noreply.github.com>
2023-08-16 09:48:53 +0100

    Initial Commit

---
+++ toasty&tasty/toasty_tastyApp.swift
@@ -0,0 +1,17 @@
+//
+//  toasty_tastyApp.swift
+//  toasty&tasty
+//
+//  Created by FURKAN AFYON on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct toasty_tastyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
